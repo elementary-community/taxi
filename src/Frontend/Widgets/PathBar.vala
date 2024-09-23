elementary-community/taxi

commit c65e7de3c157dc73986e4ab4b997df234ae65056

Port to LibSoup-3 (#84)
And thus platform 8

src/Frontend/Widgets/PathBar.vala
@@ -18,12 +18,12 @@ namespace Taxi {
     class PathBar : Gtk.Grid {
         public bool transfer_button_sensitive { get; set; }
 
-        private Soup.URI current_uri;
+        private GLib.Uri current_uri;
 
-        public signal void navigate (Soup.URI uri);
+        public signal void navigate (GLib.Uri uri);
         public signal void transfer ();
 
-        public PathBar.from_uri (Soup.URI uri) {
+        public PathBar.from_uri (GLib.Uri uri) {
             set_path (uri);
         }
 
@@ -64,13 +64,13 @@ namespace Taxi {
             button_style_context.add_class ("path-button");
 
             button.clicked.connect (() => {
-                current_uri.set_path (path);
+                current_uri.parse (current_uri.get_scheme () + path, PARSE_RELAXED);
                 navigate (current_uri);
             });
             add (button);
         }
 
-        public void set_path (Soup.URI uri) {
+        public void set_path (GLib.Uri uri) {
             clear_path ();
             current_uri = uri;
             string transfer_icon_name;

.github/workflows/ci.yml
@@ -21,7 +21,7 @@ jobs:
       fail-fast: false
 
     container:
-      image: ghcr.io/elementary/flatpak-platform/runtime:7.1-${{ matrix.arch }}
+      image: ghcr.io/elementary/flatpak-platform/runtime:8-${{ matrix.arch }}
       options: --privileged
 
     steps:

com.github.alecaddd.taxi.yml
@@ -1,7 +1,7 @@
 app-id: com.github.alecaddd.taxi
 
 runtime: io.elementary.Platform
-runtime-version: '7.1'
+runtime-version: '8'
 sdk: io.elementary.Sdk
 
 command: com.github.alecaddd.taxi

src/API/IFileAccess.vala
@@ -27,7 +27,7 @@ namespace Taxi {
          * @return true if successful at connecting
          */
         public async abstract bool connect_to_device (
-            Soup.URI uri,
+            GLib.Uri uri,
             Gtk.Window window
         );
 
@@ -38,11 +38,11 @@ namespace Taxi {
          */
         public async abstract List<FileInfo> get_file_list ();
 
-        public abstract Soup.URI get_uri ();
+        public abstract GLib.Uri get_uri ();
 
-        public abstract void goto_dir (Soup.URI uri);
+        public abstract void goto_dir (GLib.Uri uri);
 
-        public abstract void open_file (Soup.URI uri);
+        public abstract void open_file (GLib.Uri uri);
 
         public abstract File get_current_file ();
     }

src/Backend/FileAccess.vala
@@ -22,7 +22,7 @@ namespace Taxi {
         protected virtual IFileOperations file_operation { get; set; }
 
         public abstract async bool connect_to_device (
-            Soup.URI uri,
+            GLib.Uri uri,
             Gtk.Window window
         );
 
@@ -35,21 +35,21 @@ namespace Taxi {
             }
         }
 
-        public virtual Soup.URI get_uri () {
+        public virtual GLib.Uri get_uri () {
             var uri = file_handle.get_uri ();
             if (!uri.has_suffix ("/")) {
                 uri += "/";
             }
-            return new Soup.URI (uri);
+            return GLib.Uri.parse (uri, PARSE_RELAXED);
         }
 
-        public virtual void goto_dir (Soup.URI uri) {
-            var uri_string = uri.to_string (false);
+        public virtual void goto_dir (GLib.Uri uri) {
+            var uri_string = uri.to_string ();
             file_handle = File.new_for_uri (uri_string);
         }
 
-        public virtual void open_file (Soup.URI uri) {
-            var uri_string = uri.to_string (false);
+        public virtual void open_file (GLib.Uri uri) {
+            var uri_string = uri.to_string ();
             try {
                 AppInfo.launch_default_for_uri (uri_string, null);
             } catch (Error e) {

src/Backend/LocalFileAccess.vala
@@ -51,7 +51,7 @@ namespace Taxi {
         }
 
         public async override bool connect_to_device (
-            Soup.URI uri,
+            GLib.Uri uri,
             Gtk.Window window
         ) {
             return true;

src/Backend/RemoteFileAccess.vala
@@ -18,7 +18,7 @@ namespace Taxi {
 
     class RemoteFileAccess : FileAccess {
 
-        private Soup.URI uri;
+        private GLib.Uri uri;
         private Gtk.Window window;
 
         public RemoteFileAccess () {
@@ -26,13 +26,13 @@ namespace Taxi {
         }
 
         public async override bool connect_to_device (
-            Soup.URI uri,
+            GLib.Uri uri,
             Gtk.Window window
         ) {
             this.window = window;
             var mount = mount_operation_from_uri (uri);
             this.uri = uri;
-            file_handle = File.new_for_uri (uri.to_string (false));
+            file_handle = File.new_for_uri (uri.to_string ());
             try {
                 return yield file_handle.mount_enclosing_volume (
                     MountMountFlags.NONE,
@@ -86,7 +86,7 @@ namespace Taxi {
             file_handle = file_handle.resolve_relative_path ("/" + path);
         }*/
 
-        private Gtk.MountOperation mount_operation_from_uri (Soup.URI uri) {
+        private Gtk.MountOperation mount_operation_from_uri (GLib.Uri uri) {
             var mount = new Gtk.MountOperation (window);
             mount.set_domain (uri.get_host ());
             return mount;

src/Frontend/MainWindow.vala
@@ -30,7 +30,7 @@ class Taxi.MainWindow : Hdy.ApplicationWindow {
     private Granite.Widgets.Welcome welcome;
     private FilePane local_pane;
     private FilePane remote_pane;
-    private Soup.URI conn_uri;
+    private GLib.Uri conn_uri;
     private GLib.Settings saved_state;
 
     public MainWindow (
@@ -166,7 +166,7 @@ class Taxi.MainWindow : Hdy.ApplicationWindow {
         popover.operations_finished.connect (hide_spinner);
     }
 
-    private void on_connect_initiated (Soup.URI uri) {
+    private void on_connect_initiated (GLib.Uri uri) {
         show_spinner ();
         remote_access.connect_to_device.begin (uri, this, (obj, res) => {
             if (remote_access.connect_to_device.end (res)) {
@@ -177,12 +177,12 @@ class Taxi.MainWindow : Hdy.ApplicationWindow {
                 update_pane (Location.LOCAL);
                 update_pane (Location.REMOTE);
                 connect_box.show_favorite_icon (
-                    conn_saver.is_bookmarked (remote_access.get_uri ().to_string (false))
+                    conn_saver.is_bookmarked (remote_access.get_uri ().to_string ())
                 );
                 conn_uri = uri;
             } else {
                 alert_stack.visible_child = welcome;
-                welcome.title = _("Could not connect to '%s'").printf (uri.to_string (false));
+                welcome.title = _("Could not connect to '%s'").printf (uri.to_string ());
             }
             hide_spinner ();
         });
@@ -197,7 +197,7 @@ class Taxi.MainWindow : Hdy.ApplicationWindow {
     }
 
     private void bookmark () {
-        var uri_string = conn_uri.to_string (false);
+        var uri_string = conn_uri.to_string ();
         if (conn_saver.is_bookmarked (uri_string)) {
             conn_saver.remove (uri_string);
         } else {
@@ -233,19 +233,19 @@ class Taxi.MainWindow : Hdy.ApplicationWindow {
         }
     }
 
-    private void on_local_navigate (Soup.URI uri) {
+    private void on_local_navigate (GLib.Uri uri) {
         navigate (uri, local_access, Location.LOCAL);
     }
 
-    private void on_local_open (Soup.URI uri) {
+    private void on_local_open (GLib.Uri uri) {
         local_access.open_file (uri);
     }
 
-    private void on_remote_navigate (Soup.URI uri) {
+    private void on_remote_navigate (GLib.Uri uri) {
         navigate (uri, remote_access, Location.REMOTE);
     }
 
-    private void on_remote_open (Soup.URI uri) {
+    private void on_remote_open (GLib.Uri uri) {
         remote_access.open_file (uri);
     }
 
@@ -257,15 +257,15 @@ class Taxi.MainWindow : Hdy.ApplicationWindow {
         file_dragged (uri, Location.LOCAL, local_access);
     }
 
-    private void on_local_file_delete (Soup.URI uri) {
+    private void on_local_file_delete (GLib.Uri uri) {
         file_delete (uri, Location.LOCAL);
     }
 
-    private void on_remote_file_delete (Soup.URI uri) {
+    private void on_remote_file_delete (GLib.Uri uri) {
         file_delete (uri, Location.REMOTE);
     }
 
-    private void navigate (Soup.URI uri, IFileAccess file_access, Location pane) {
+    private void navigate (GLib.Uri uri, IFileAccess file_access, Location pane) {
         file_access.goto_dir (uri);
         update_pane (pane);
     }
@@ -294,8 +294,8 @@ class Taxi.MainWindow : Hdy.ApplicationWindow {
          );
     }
 
-    private void file_delete (Soup.URI uri, Location pane) {
-        var file = File.new_for_uri (uri.to_string (false));
+    private void file_delete (GLib.Uri uri, Location pane) {
+        var file = File.new_for_uri (uri.to_string ());
         file_operation.delete_recursive.begin (
             file,
             new Cancellable (),
@@ -335,7 +335,7 @@ class Taxi.MainWindow : Hdy.ApplicationWindow {
         });
     }
 
-    private Soup.URI on_ask_hostname () {
+    private GLib.Uri on_ask_hostname () {
         return conn_uri;
     }
 

commit 034397b95a4bf5a409c7da5849950a84dbda028e

Pathbar: fix invalid uri on button clicked (#87)
Regression introduced in #84

src/Frontend/Widgets/PathBar.vala
@@ -64,7 +64,7 @@ namespace Taxi {
             button_style_context.add_class ("path-button");
 
             button.clicked.connect (() => {
-                current_uri.parse (current_uri.get_scheme () + path, PARSE_RELAXED);
+                current_uri = GLib.Uri.parse (current_uri.get_scheme () + "://" + path, PARSE_RELAXED);
                 navigate (current_uri);
             });
             add (button);